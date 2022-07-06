85aa0bb7ecc58c51673014aa2d767864037253c2
Franciane Pires <franpp22@gmail.com>
2022-07-06 17:57:19 -0300

    testando editar

--- javascript/form.js
+++ javascript/form.js
@@ -8,13 +8,44 @@ function testform(e) {
         var people = []
     }
 
-    people.push ({
-    name: e.target.elements['name'].value,
-    tel: e.target.elements['phone'].value,
-    xp: (e.target.elements['xp'].value == 'true')
-    })
+    if (id !== null) {
+        people[id] = {
+            name: e.target.elements['name'].value,
+            tel: e.target.elements['phone'].value,
+            xp: (e.target.elements['xp'].value == 'true')
+            }
+        
+    } else {
+        people.push ({
+        name: e.target.elements['name'].value,
+        tel: e.target.elements['phone'].value,
+        xp: (e.target.elements['xp'].value == 'true')
+        }) 
+    }
 
     localStorage.setItem('people', JSON.stringify(people))
 
     document.getElementById('goHome').click()
+}
+
+var urlPrincipal = new URL(window.location.href)
+
+var id = urlPrincipal.searchParams.get('person')
+if (id !== null) {
+    var peopleRaw = localStorage.getItem('people')
+    if(peopleRaw != null) { 
+        var people = JSON.parse(peopleRaw)
+    } else {
+        var people = []
+    }
+    console.log(people[id])
+
+    document.getElementById('name').value = people[id].name
+    document.getElementById('phone').value = people[id].tel
+    if (people[id].xp) {
+        document.getElementById('xp-yes').checked = true 
+    } else { 
+        document.getElementById('xp-no').checked = false  
+    }
+
 }
