e676756a58e3d796d05f90d42f557254a62163f8
Franciane Pires <franpp22@gmail.com>
2022-07-06 14:53:58 -0300

    alterando html e inicio do js

---
+++ javascript/index.js
@@ -0,0 +1,47 @@
+var people = [
+    { 
+        name: 'Fernando Henrique Caversan Santos Duro',
+        tel: '14999999999',
+        xp: true
+    }, 
+    { 
+        name: 'Franciane Paiva Pires',
+        tel: '14999999999',
+        xp: false
+    },
+    {
+        name: 'Andersan Arcenio Matos da Costa',
+        tel: '14999999999',
+        xp: true
+    },
+    {
+        name: 'Karina do Amaral',
+        tel: '14999999999',
+        xp: false
+    }, 
+    {
+        name: 'Cauê Sanches Padula',
+        tel: '14999999999',
+        xp: true
+    },
+]
+
+
+for (person in people) { 
+    console.log(person)
+    document.querySelector('table.list tbody').innerHTML += 
+        `<tr style="background-color: ${((person % 2 == 0) ? '#fff' : '#eee')}"> 
+            <td> 
+                ${ people[person].name }
+            </td>
+            <td> 
+                ${ people[person].tel }
+            </td>
+            <td> 
+                ${ (people[person].xp ? '<strong style="color:green"> Sim </strong>' : '<strong style="color:red"> Não </strong>' )} 
+            </td>
+            <td> 
+                <button> Excluir </button>
+            </td> 
+        </tr>`
+}
